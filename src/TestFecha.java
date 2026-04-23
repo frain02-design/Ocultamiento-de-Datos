f568a334100980cc3f7881738b8c2a90079af8dc
Felipe Rain <felipeysmaelrain@gmail.com>
2026-04-22 23:39:52 -0400

    test: agregar clase TestFecha con método main
    - Crea instancia de TodaysDate llamada 'hoy'
    - Ejecuta método printDateAndTime() para mostrar fecha y hora
    - Clase principal del programa

--- src/TestFecha.java
+++ src/TestFecha.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+public class TestFecha import myUtilities.TodaysDate;
 
 public class TestFecha {
+    public static void main(String[] args) {
+        TodaysDate hoy = new TodaysDate();
+        hoy.printDateAndTime();
+    }
+}{
 }
